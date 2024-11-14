06db3690526c0782453bf9efabce7019d9b1dc01
JosephGoff <joeygoff13@gmail.com>
2024-11-13 19:49:21 -0500

    11/13/24 fixed inspire connect to use railway

--- connect.js
+++ connect.js
@@ -1,20 +1,20 @@
-import mysql from "mysql"
+import mysql from "mysql2"
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const db = mysql.createConnection({
-    host: "aws-db2.c9wkcmm26naa.us-east-2.rds.amazonaws.com",
-    user: "admin",
-    password: process.env.DB_PW,
-    database: "online_store"
-})
+// export const db = mysql.createConnection({
+//     host: "aws-db2.c9wkcmm26naa.us-east-2.rds.amazonaws.com",
+//     user: "admin",
+//     password: process.env.DB_PW,
+//     database: "online_store"
+// })
 
-export const db_reis = mysql.createConnection({
-    host: "aws-db2.c9wkcmm26naa.us-east-2.rds.amazonaws.com",
-    user: "admin",
-    password: process.env.DB_PW,
-    database: "reis"
-})
+// export const db_reis = mysql.createConnection({
+//     host: "aws-db2.c9wkcmm26naa.us-east-2.rds.amazonaws.com",
+//     user: "admin",
+//     password: process.env.DB_PW,
+//     database: "reis"
+// })
 
 // export const db_social = mysql.createConnection({
 //     host: "aws-db2.c9wkcmm26naa.us-east-2.rds.amazonaws.com",
@@ -30,3 +30,6 @@ export const db_social = mysql.createConnection({
     password: process.env.MYSQL_PASSWORD, 
     database: process.env.MYSQL_DATABASE 
 });
+
+export const db_reis = db_social
+export const db = db_social
